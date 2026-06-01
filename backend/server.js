261b426b75bf140b44759121cc062d597dc137ec
apple <apple@apples-MacBook-Air.local>
2026-06-01 16:07:22 +0530

    Fix CORS for Vercel

--- backend/server.js
+++ backend/server.js
@@ -9,14 +9,17 @@ dotenv.config();
 const app = express();
 
 // ✅ CORS
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "http://localhost:5174"
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://hospital-management-vert-one.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 // ✅ VERY IMPORTANT (UNCOMMENT THIS)
 app.use(express.json());
 
